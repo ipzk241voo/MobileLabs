717162d767604f8cdf52aa1aca5c7c4ae3cf3fc6
ipzk241voo <ipzk241_voo@student.ztu.edu.ua>
2025-05-23 07:01:04 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxO6LuUhRWvO7wXBQqKBX2_S7fQrbeRCA",
-  authDomain: "ndpowerlab6.firebaseapp.com",
-  projectId: "ndpowerlab6",
-  storageBucket: "ndpowerlab6.firebasestorage.app",
+  apiKey: "****************************",
+  authDomain: "*.firebaseapp.com",
+  projectId: "*",
+  storageBucket: "*.firebasestorage.app",
   messagingSenderId: "655206773109",
-  appId: "1:655206773109:web:f0a3c7ec27170b881b233c",
-  measurementId: "G-E2QWW0QE78"
+  appId: "***********************",
+  measurementId: "***************"
 
 };
 
